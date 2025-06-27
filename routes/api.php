27618d67b2a5a4d99a87b939a524926a5a5148c4
cpdmo@moeys.gov.kh <cpdmo@moeys.gov.kh>
2025-06-27 16:03:09 +0700

    c12

--- routes/api.php
+++ routes/api.php
@@ -5,28 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\AuthController;
 use App\Http\Controllers\Api\Admin\UserController;
 
-$admin_public_path = 'admin-public';//Public API
-$admin_secure_path = 'admin-secure';//Secure API (Require Token)
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
-Route::get($admin_public_path, function () {
-    return response()->json([
-        "Api welocme to laravel"
-    ]);
-});
-
-$admin_public_path = 'admin-public';//Public API
 
 Route::prefix('admin-public')->group(function () {
-
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login',    [AuthController::class, 'login']);
 });
 
-Route::prefix($admin_secure_path)->middleware('auth:sanctum')->group(function () {
+Route::prefix('admin-secure')->middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [UserController::class, 'profile']);
 });
