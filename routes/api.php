2fce8b402afb98895ba455773b4c447e9c392ab0
limvichet <83205736+limvichet@users.noreply.github.com>
2025-07-07 15:17:52 +0700

    c api 20250707

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\AuthController;
 use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Api\Admin\LocationController;
 
 
 /* public */
@@ -17,4 +18,17 @@ Route::prefix('admin-public')->group(function () {
 Route::prefix('admin-secure')->middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [UserController::class, 'profile']);
+
+
+    Route::get('/locations/get-provinces', [LocationController::class, 'getProvinces']);
+    Route::get('/locations/get-districts', [LocationController::class, 'getDistricts']);
+    Route::get('/locations/get-communes', [LocationController::class, 'getCommunes']);
+    Route::get('/locations/get-villages', [LocationController::class, 'getVillages']);
+
+    Route::get('/locations/get-locations', [LocationController::class, 'getLocations']);
+    Route::get('/locations/get-location-types', [LocationController::class, 'getLocationTypes']);
+    Route::get('/locations/get-location-regions', [LocationController::class, 'getLocationRegions']);
+    Route::get('/locations/get-location-levels', [LocationController::class, 'getLocationLevels']);
+
+
 });
